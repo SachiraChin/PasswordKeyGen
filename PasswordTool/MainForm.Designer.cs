5d5dd0b7e3171b3139488c6d90572a9658a1a89b
Sachira <Sachira@SACHIRAPC>
2016-08-15 22:34:07 +0530

    changes added to generate hash for given password

--- PasswordTool/MainForm.Designer.cs
+++ PasswordTool/MainForm.Designer.cs
@@ -44,20 +44,23 @@
             this.label6 = new System.Windows.Forms.Label();
             this.ClientIdTextBox = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
+            this.GeneratePasswordWithHashButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // PasswordTextBox
             // 
-            this.PasswordTextBox.Location = new System.Drawing.Point(12, 34);
+            this.PasswordTextBox.Location = new System.Drawing.Point(18, 52);
+            this.PasswordTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.PasswordTextBox.Name = "PasswordTextBox";
-            this.PasswordTextBox.Size = new System.Drawing.Size(167, 20);
+            this.PasswordTextBox.Size = new System.Drawing.Size(248, 26);
             this.PasswordTextBox.TabIndex = 0;
             // 
             // GeneratePasswordHashButton
             // 
-            this.GeneratePasswordHashButton.Location = new System.Drawing.Point(195, 32);
+            this.GeneratePasswordHashButton.Location = new System.Drawing.Point(594, 48);
+            this.GeneratePasswordHashButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.GeneratePasswordHashButton.Name = "GeneratePasswordHashButton";
-            this.GeneratePasswordHashButton.Size = new System.Drawing.Size(184, 23);
+            this.GeneratePasswordHashButton.Size = new System.Drawing.Size(276, 35);
             this.GeneratePasswordHashButton.TabIndex = 1;
             this.GeneratePasswordHashButton.Text = "Generate Password Hash";
             this.GeneratePasswordHashButton.UseVisualStyleBackColor = true;
@@ -65,30 +68,34 @@
             // 
             // PasswordHashTextBox
             // 
-            this.PasswordHashTextBox.Location = new System.Drawing.Point(12, 83);
+            this.PasswordHashTextBox.Location = new System.Drawing.Point(18, 128);
+            this.PasswordHashTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.PasswordHashTextBox.Name = "PasswordHashTextBox";
-            this.PasswordHashTextBox.Size = new System.Drawing.Size(480, 20);
+            this.PasswordHashTextBox.Size = new System.Drawing.Size(718, 26);
             this.PasswordHashTextBox.TabIndex = 2;
             // 
             // PasswordSaltTextBox
             // 
-            this.PasswordSaltTextBox.Location = new System.Drawing.Point(12, 127);
+            this.PasswordSaltTextBox.Location = new System.Drawing.Point(18, 195);
+            this.PasswordSaltTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.PasswordSaltTextBox.Name = "PasswordSaltTextBox";
-            this.PasswordSaltTextBox.Size = new System.Drawing.Size(480, 20);
+            this.PasswordSaltTextBox.Size = new System.Drawing.Size(718, 26);
             this.PasswordSaltTextBox.TabIndex = 3;
             // 
             // ClientSecretTextBox
             // 
-            this.ClientSecretTextBox.Location = new System.Drawing.Point(12, 266);
+            this.ClientSecretTextBox.Location = new System.Drawing.Point(18, 409);
+            this.ClientSecretTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.ClientSecretTextBox.Name = "ClientSecretTextBox";
-            this.ClientSecretTextBox.Size = new System.Drawing.Size(480, 20);
+            this.ClientSecretTextBox.Size = new System.Drawing.Size(718, 26);
             this.ClientSecretTextBox.TabIndex = 4;
             // 
             // GenerateClientSecretButton
             // 
-            this.GenerateClientSecretButton.Location = new System.Drawing.Point(12, 173);
+            this.GenerateClientSecretButton.Location = new System.Drawing.Point(18, 266);
+            this.GenerateClientSecretButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.GenerateClientSecretButton.Name = "GenerateClientSecretButton";
-            this.GenerateClientSecretButton.Size = new System.Drawing.Size(178, 23);
+            this.GenerateClientSecretButton.Size = new System.Drawing.Size(267, 35);
             this.GenerateClientSecretButton.TabIndex = 5;
             this.GenerateClientSecretButton.Text = "Generate Client Secret";
             this.GenerateClientSecretButton.UseVisualStyleBackColor = true;
@@ -97,92 +104,114 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(18, 14);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.Size = new System.Drawing.Size(78, 20);
             this.label1.TabIndex = 6;
             this.label1.Text = "Password";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 67);
+            this.label2.Location = new System.Drawing.Point(18, 103);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.Size = new System.Drawing.Size(47, 20);
             this.label2.TabIndex = 6;
             this.label2.Text = "Hash";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 111);
+            this.label3.Location = new System.Drawing.Point(18, 171);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(25, 13);
+            this.label3.Size = new System.Drawing.Size(37, 20);
             this.label3.TabIndex = 6;
             this.label3.Text = "Salt";
             // 
             // ClientSecretHashTextBox
             // 
-            this.ClientSecretHashTextBox.Location = new System.Drawing.Point(12, 310);
+            this.ClientSecretHashTextBox.Location = new System.Drawing.Point(18, 477);
+            this.ClientSecretHashTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.ClientSecretHashTextBox.Name = "ClientSecretHashTextBox";
-            this.ClientSecretHashTextBox.Size = new System.Drawing.Size(480, 20);
+            this.ClientSecretHashTextBox.Size = new System.Drawing.Size(718, 26);
             this.ClientSecretHashTextBox.TabIndex = 7;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 250);
+            this.label4.Location = new System.Drawing.Point(18, 385);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.Size = new System.Drawing.Size(100, 20);
             this.label4.TabIndex = 6;
             this.label4.Text = "Client Secret";
             // 
             // ClientSecretSaltTextBox
             // 
-            this.ClientSecretSaltTextBox.Location = new System.Drawing.Point(12, 354);
+            this.ClientSecretSaltTextBox.Location = new System.Drawing.Point(18, 545);
+            this.ClientSecretSaltTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.ClientSecretSaltTextBox.Name = "ClientSecretSaltTextBox";
-            this.ClientSecretSaltTextBox.Size = new System.Drawing.Size(480, 20);
+            this.ClientSecretSaltTextBox.Size = new System.Drawing.Size(718, 26);
             this.ClientSecretSaltTextBox.TabIndex = 8;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 294);
+            this.label5.Location = new System.Drawing.Point(18, 452);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.Size = new System.Drawing.Size(47, 20);
             this.label5.TabIndex = 6;
             this.label5.Text = "Hash";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(12, 338);
+            this.label6.Location = new System.Drawing.Point(18, 520);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(25, 13);
+            this.label6.Size = new System.Drawing.Size(37, 20);
             this.label6.TabIndex = 6;
             this.label6.Text = "Salt";
             // 
             // ClientIdTextBox
             // 
-            this.ClientIdTextBox.Location = new System.Drawing.Point(12, 227);
+            this.ClientIdTextBox.Location = new System.Drawing.Point(18, 349);
+            this.ClientIdTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.ClientIdTextBox.Name = "ClientIdTextBox";
-            this.ClientIdTextBox.Size = new System.Drawing.Size(480, 20);
+            this.ClientIdTextBox.Size = new System.Drawing.Size(718, 26);
             this.ClientIdTextBox.TabIndex = 9;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(12, 211);
+            this.label7.Location = new System.Drawing.Point(18, 325);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(45, 13);
+            this.label7.Size = new System.Drawing.Size(67, 20);
             this.label7.TabIndex = 6;
             this.label7.Text = "Client Id";
             // 
+            // GeneratePasswordWithHashButton
+            // 
+            this.GeneratePasswordWithHashButton.Location = new System.Drawing.Point(289, 48);
+            this.GeneratePasswordWithHashButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.GeneratePasswordWithHashButton.Name = "GeneratePasswordWithHashButton";
+            this.GeneratePasswordWithHashButton.Size = new System.Drawing.Size(276, 35);
+            this.GeneratePasswordWithHashButton.TabIndex = 10;
+            this.GeneratePasswordWithHashButton.Text = "Generate Password with Hash";
+            this.GeneratePasswordWithHashButton.UseVisualStyleBackColor = true;
+            this.GeneratePasswordWithHashButton.Click += new System.EventHandler(this.GeneratePasswordWithHashButton_Click);
+            // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(502, 394);
+            this.ClientSize = new System.Drawing.Size(883, 606);
+            this.Controls.Add(this.GeneratePasswordWithHashButton);
             this.Controls.Add(this.ClientIdTextBox);
             this.Controls.Add(this.ClientSecretSaltTextBox);
             this.Controls.Add(this.ClientSecretHashTextBox);
@@ -199,6 +228,7 @@
             this.Controls.Add(this.PasswordHashTextBox);
             this.Controls.Add(this.GeneratePasswordHashButton);
             this.Controls.Add(this.PasswordTextBox);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "MainForm";
             this.Text = "Password Genration Tool";
             this.ResumeLayout(false);
@@ -224,6 +254,7 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox ClientIdTextBox;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button GeneratePasswordWithHashButton;
     }
 }
 
--- PasswordTool/MainForm.cs
+++ PasswordTool/MainForm.cs
@@ -10,15 +10,7 @@ namespace PasswordTool
         {
             InitializeComponent();
         }
-
-        private void GeneratePasswordHashButton_Click(object sender, EventArgs e)
-        {
-            PasswordTextBox.Text = System.Web.Security.Membership.GeneratePassword(16, 6);
-            var hash = PasswordHashProvider.CreateHash(PasswordTextBox.Text);
-            PasswordHashTextBox.Text = hash.Hash;
-            PasswordSaltTextBox.Text = hash.Salt;
-        }
-
+        
         private void GenerateClientSecretButton_Click(object sender, EventArgs e)
         {
             ClientIdTextBox.Text= Guid.NewGuid().ToString("N");
@@ -27,6 +19,30 @@ namespace PasswordTool
             ClientSecretTextBox.Text = secret;
             ClientSecretHashTextBox.Text = hash.Hash;
             ClientSecretSaltTextBox.Text = hash.Salt;
+        }
+        
+        private void GeneratePasswordWithHashButton_Click(object sender, EventArgs e)
+        {
+            while (true)
+            {
+                var pass = System.Web.Security.Membership.GeneratePassword(16, 6);
+
+                if (pass.Contains(":") || pass.Contains("&") || pass.Contains(".") || pass.Contains("%"))
+                    continue;
+
+                PasswordTextBox.Text = pass;
+                break;
+            }
+            var hash = PasswordHashProvider.CreateHash(PasswordTextBox.Text);
+            PasswordHashTextBox.Text = hash.Hash;
+            PasswordSaltTextBox.Text = hash.Salt;
+        }
+
+        private void GeneratePasswordHashButton_Click(object sender, EventArgs e)
+        {
+            var hash = PasswordHashProvider.CreateHash(PasswordTextBox.Text);
+            PasswordHashTextBox.Text = hash.Hash;
+            PasswordSaltTextBox.Text = hash.Salt;
         }
     }
 }
